41506bd27efc66f0ba83d0fb3f31e63a1c53d507
licaakwarteng <angelicaakwarteng24@gmail.com>
2026-04-05 16:45:27 +0000

    Renamed streamlit.py to streamlitapp.py and saved binary model to pickle file.

---
+++ streamlitapp.py
@@ -0,0 +1,57 @@
+import numpy as np
+import sklearn
+import pickle
+import pandas as pd
+import streamlit as st
+
+#loading model which was saved
+with open('trained_model.pkl', 'rb') as file:
+    loaded_model = pickle.load(file)
+
+# making predictions
+def loan_default_prediction(input_data):
+
+    #load my input data
+    input_data = pd.to_numeric(input_data)
+    input_data_reshaped = input_data.reshape(1, -1)
+
+    prediction = loaded_model.predict(input_data_reshaped)
+    if prediction[0] == 0:
+        return 'Less likely to default'
+    else:
+        return 'Likely to default'
+
+
+
+def main():
+    # setting the title
+    st.title('Credit Risk Decision System App')
+    
+    # load my input data
+
+    income_per_credit_line = st.text_input('Income per credit line', key='income_per_credit_line')
+    loan_purpose_house = st.text_input('Loan purpose if house. Yes: 1 No: 0', key='Enter 1 if yes else 0')
+    loan_purpose_credit_card = st.text_input('Loan purpose if credit card. Yes: 1 No: 0', key='loan_purpose_credit_card')
+    inquiries_last_12m = st.text_input('Inquiries made in the last 12 months', key='inquiries_last_12m')
+    term = st.text_input('Loan term', key='term')
+    num_total_cc_accounts = st.text_input('Number of current accounts', key='num_total_cc_accounts')
+    months_since_last_credit_inquiry = st.text_input('Months since last credit inquiry', key='months_since_last_credit_inquiry')
+    num_open_cc_accounts = st.text_input('Number of open accounts', key='num_open_cc_accounts')
+    open_credit_lines = st.text_input('Number of open credit lines', key='open_credit_lines')
+    num_satisfactory_accounts = st.text_input('Number of satisfactory accounts', key='num_satisfactory_accounts')
+    loan_amount = st.text_input('Loan amount', key='loan_amount')
+    application_type_joint = st.text_input('Application Type: Individual/ Joint. Yes: 1 No: 0', key='application_type_joint')
+
+    #prediction variable
+    prediction = ''
+
+    #predicting loan default    
+    if st.button('Loan Default Test Result'):
+        prediction = loan_default_prediction([income_per_credit_line,loan_purpose_house,loan_purpose_credit_card,inquiries_last_12m,term,num_total_cc_accounts,months_since_last_credit_inquiry,num_open_cc_accounts,open_credit_lines,num_satisfactory_accounts,loan_amount,application_type_joint])
+
+    st.success(prediction)
+
+
+
+if __name__ == '__main__':
+    main()
